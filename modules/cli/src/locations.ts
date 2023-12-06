2c9edcc08c84c7bb55c62f4d07edcec601bf2de3
phil <phil.rice@validoc.org>
2023-12-06 18:20:32 +0100

    ensuring locations have a unique entity name

--- modules/cli/src/locations.ts
+++ modules/cli/src/locations.ts
@@ -28,12 +28,13 @@ function findLocationFileData ( roots: Tree<ModuleDataWithoutErrors>[], defaultN
   } )
   const existingRootIndex = roots.findIndex ( md => moduleDataPath ( md.value ) === '.' )
   const existingRoot = roots[ existingRootIndex ]
-  const validExistingRoot = existingRoot && !hasErrors ( existingRoot.value ) && !existingRoot.value.ignore
+  const validExistingRoot = existingRoot && !hasErrors ( existingRoot.value ) && isNotExistingGenerated(existingRoot.value)
+  // console.log('findLocationFileData', { existingRootIndex, existingRoot, validExistingRoot, defaultName, roots })
   if ( !validExistingRoot ) {
     if ( existingRootIndex !== -1 ) result.splice ( existingRootIndex, 1 )
     if ( defaultName === undefined ) result.push ( [ `No name for root` ] )
     else {
-      const newRoot = { path: './catalog-info.yaml', name: defaultName, children: roots.map ( root => prefixWithDotIfNeeded ( root.value.catalogName ) ) };
+      const newRoot = { path: './catalog-info.yaml', name: defaultName, children: roots.map ( root => prefixWithDotIfNeeded ( path.dirname(root.value.catalogName) + '/catalog-info-yaml' ) ) };
       result.push ( newRoot )
     }
   }
@@ -41,7 +42,7 @@ function findLocationFileData ( roots: Tree<ModuleDataWithoutErrors>[], defaultN
 }
 
 export function findRoots ( mds: ModuleData[], all: boolean ): Tree<ModuleDataWithoutErrors>[] {
-  const filtered = mds.filter ( md => !hasErrors ( md ) ).filter ( isNotExistingGenerated ) as ModuleDataWithoutErrors[]
+  const filtered = mds.filter ( md => !hasErrors ( md )).filter(isNotExistingGenerated) as ModuleDataWithoutErrors[]
   const tree = makeTreeFromPathFnAndArray<ModuleDataWithoutErrors> ( moduleDataPath, filtered )
   const roots = Object.values ( tree ).filter ( t => t.parent === undefined && !hasErrors ( t.value ) ) as Tree<ModuleDataWithoutErrors>[]
   return roots;
@@ -65,6 +66,7 @@ export function makeLocationFiles ( mds: ModuleData[], template: string, name: s
       value: derefence ( `Location file for ${path}`, dic, template, { variableDefn: doubleXmlVariableDefn } )
     });
   } ) );
+  // console.log('makeLocationFiles', result)
   return result;
 }
 
